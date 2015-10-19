93df92c6f928b108e958bbae1cf8a0c88992268a
Nola Stowe <nola.stowe@ge.com>
2015-10-19 16:09:51 -0500

    added tests for build and publish functions of template

--- src/clj_tosca/template.clj
+++ src/clj_tosca/template.clj
@@ -5,4 +5,13 @@
             [clj-yaml.core :as yaml]))
 
 (defn build [& nodes]
-  (merge {:tosca_definitions_version "tosca_simple_yaml_1_0"} (apply merge nodes))) 
+  (apply merge nodes)) 
+
+(defn publish ([nodes]
+               (publish nodes "json"))
+              ([nodes format]
+                (let [template (merge {:tosca_definitions_version "tosca_simple_yaml_1_0"} nodes)]
+                  (case format
+                    "yaml" (yaml/generate-string template)
+                    "edn"  (pr-str template)
+                    "json" (json/write-str template)))))
---
+++ test/clj_tosca/template_test.clj
@@ -0,0 +1,28 @@
+(ns clj-tosca.template-test
+  (:require [clj-tosca.template :as template]
+            [clj-tosca.node :as node]
+            [clj-tosca.node-instance :as nodei]
+            [clojure.test :refer :all]))
+
+(deftest merging-nodes
+  (let [result (template/build {:one 1} {:two 2})]
+    (is (= result {:one 1 :two 2}))))
+
+(deftest merging-complex-nodes
+  (let [node (-> (node/build)
+                 (node/add-property "system" "on"))
+        nodei (-> (nodei/build)
+                  (nodei/add-property "state" "pending"))
+        result (template/build nodei node)]
+    (is (= result {:node_instance {:properties {:state "pending"}}, :node_types {:ServerNode {:type "tosca.nodes.Compute", :properties {:system "on"}, :attributes {}, :requirements {}, :capabilities {}, :interfaces {}, :artifacts {}, :metadatas {}}}}))))
+
+(deftest publishing-template
+  (let [node (-> (node/build)
+                 (node/add-property "system" "on"))
+        nodei (-> (nodei/build)
+                  (nodei/add-property "state" "pending"))
+        template (template/build nodei node)
+        result (template/publish template)]
+    (is (= result "{\"tosca_definitions_version\":\"tosca_simple_yaml_1_0\",\"node_instance\":{\"properties\":{\"state\":\"pending\"}},\"node_types\":{\"ServerNode\":{\"type\":\"tosca.nodes.Compute\",\"properties\":{\"system\":\"on\"},\"attributes\":{},\"requirements\":{},\"capabilities\":{},\"interfaces\":{},\"artifacts\":{},\"metadatas\":{}}}}"))))
+
+
