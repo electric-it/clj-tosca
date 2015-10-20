dd8f3df9317e6ba15ad534e8e2597d9223130619
Nola Stowe <nola.stowe@ge.com>
2015-10-20 18:39:20 -0500

    adding generated docs here for now

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-yaml "0.4.0"]])
+                 [clj-yaml "0.4.0"]]
+  :codox {:output-path "codox"})
