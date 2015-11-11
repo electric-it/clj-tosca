0a1054e5182acfbc7f2b74cedbbb3c8c79a2f057
Nola Stowe <nola.stowe@ge.com>
2015-11-11 15:25:27 -0600

    bumped version number

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject org.clojars.electric-it.clj-tosca "0.1.0-SNAPSHOT"
+(defproject electric-it.clj-tosca "1.0.0"
   :description "A tool to build TOSCA documents"
   :license "Apache Version 2.0"
   :url "https://github.com/electric-it/clj-tosca"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-yaml "0.4.0"]]
-  :codox {:output-path "codox"})
+                 [clj-yaml "0.4.0"]])
