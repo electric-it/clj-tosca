7e52d10d40595f1b6cdaec246aeaad3f206d1798
Nola Stowe <nola.stowe@ge.com>
2015-11-11 15:18:25 -0600

    updated license and url

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject clj-tosca "0.1.0-SNAPSHOT"
+(defproject org.clojars.electric-it.clj-tosca "0.1.0-SNAPSHOT"
   :description "A tool to build TOSCA documents"
+  :license "Apache Version 2.0"
+  :url "https://github.com/electric-it/clj-tosca"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-yaml "0.4.0"]]
