701fd06408bb4e61ef57dbb122f0c8620997f8bb
Nola Stowe <nola.stowe@ge.com>
2015-12-29 11:42:16 -0600

    updated name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject electric-it.clj-tosca "1.0.0"
+(defproject electric-it/clj-tosca "1.0.0"
   :description "A tool to build TOSCA documents"
   :license "Apache Version 2.0"
   :url "https://github.com/electric-it/clj-tosca"
