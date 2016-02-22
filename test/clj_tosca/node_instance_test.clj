ae9a81eebd681c46f583526e21ab611c94c30df4
Nola Stowe <nola.stowe@ge.com>
2016-02-22 17:14:53 -0600

    refatored tests to be more better

--- test/clj_tosca/node_instance_test.clj
+++ test/clj_tosca/node_instance_test.clj
@@ -1,23 +1,22 @@
 (ns clj-tosca.node-instance-test
-  (:require [clj-tosca.node-instance :as nodei]
+  (:require [clj-tosca.tosca :refer :all]
             [clojure.test :refer :all]))
 
-
 (deftest builds-instance-node-is-map
-  (let [node (nodei/build)]
+  (let [node (instance-build)]
     (is (= (class node) clojure.lang.PersistentArrayMap))))
 
 (deftest builds-instance-nodes-init
-  (let [node (nodei/build)]
+  (let [node (instance-build)]
     (is (= node {:node_instance {:properties {}}}))))
 
 (deftest adds-state
-  (let [node (-> (nodei/build)
-                 (nodei/add-state "started"))]
+  (let [node (-> (instance-build)
+                 (instance-add-state "started"))]
     (is (= node {:node_instance {:properties {} :state "started"}}))))
 
 (deftest adds-properties
-  (let [node (-> (nodei/build)
-                 (nodei/add-property "instanceName" "ERTA132")
-                 (nodei/add-property "region" "us-east-1"))]
+  (let [node (-> (instance-build)
+                 (instance-add-property "instanceName" "ERTA132")
+                 (instance-add-property "region" "us-east-1"))]
     (is (= node {:node_instance {:properties {:instanceName "ERTA132" :region "us-east-1"}}}))))
--- test/clj_tosca/node_test.clj
+++ test/clj_tosca/node_test.clj
@@ -1,104 +1,86 @@
 (ns clj-tosca.node-test
-  (:require [clj-tosca.node :as node]
+  (:require [clj-tosca.tosca :refer :all]
             [clojure.test :refer :all]))
 
 (deftest test-node-creates-returns-map
-  (let [node (node/build)]
+  (let [node (node-build)]
     (is (= (class node) clojure.lang.PersistentArrayMap))))
 
 (deftest test-node-has-key-node-types
-  (let [node (node/build)]
+  (let [node (node-build)]
     (is (= (first (keys node)) :node_types))))
 
 (deftest test-type-is-set
-  (let [node (node/build)]
+  (let [node (node-build)]
     (is (= (:type (:Node (:node_types node))) "tosca.nodes.Compute"))))
 
 (deftest test-adding-one-property
-  (let [node (-> (node/build)
-                 (node/add-property "state" "starting"))
-        added-props (->> (:node_types node)
-                         (:Node)
-                         (:properties))]
-    (is (= added-props {:state "starting"}))))
+  (let [node (-> (node-build)
+                 (node-add-property "state" "starting"))
+        expected-props (get-in node [:node_types :Node :properties])]
+    (is (= expected-props {:state "starting"}))))
 
 (deftest test-adding-two-properties
-  (let [node (-> (node/build)
-                 (node/add-property "state" "starting")
-                 (node/add-property "size" 1))
-        added-props (->> (:node_types node)
-                         (:Node)
-                         (:properties))]
-    (is (= added-props {:state "starting", :size 1}))))
+  (let [node (-> (node-build)
+                 (node-add-property "state" "starting")
+                 (node-add-property "size" 1))
+        expected-props (get-in node [:node_types :Node :properties])]
+    (is (= expected-props {:state "starting", :size 1}))))
 
 (deftest test-adding-one-attribute
-  (let [node (-> (node/build)
-                 (node/add-attribute "size" "medium"))
-        added-attrs (->> (:node_types node)
-                         (:Node)
-                         (:attributes))]
-    (is (= added-attrs {:size "medium"}))))
+  (let [node (-> (node-build)
+                 (node-add-attribute "size" "medium"))
+        expected-attrs (get-in node [:node_types :Node :attributes])]
+    (is (= expected-attrs {:size "medium"}))))
 
 (deftest test-adding-two-attributes
-  (let [node (-> (node/build)
-                 (node/add-attribute "size" "medium"))
-        node-w-attrs (node/add-attribute node "memory" "8gb")
-        added-attrs (->> (:node_types node-w-attrs)
-                         (:Node)
-                         (:attributes))]
-    (is (= added-attrs {:size "medium" :memory "8gb"}))))
+  (let [node (-> (node-build)
+                 (node-add-attribute "size" "medium")
+                 (node-add-attribute "memory" "8gb"))
+        expected-attrs (get-in node [:node_types :Node :attributes])]
+    (is (= expected-attrs {:size "medium" :memory "8gb"}))))
 
 (deftest test-adding-one-requirement
-  (let [node (-> (node/build)
-                 (node/add-requirement "SLA" "Level 1"))
-        added-req (->> (:node_types node)
-                       (:Node)
-                       (:requirements))]
-    (is (= added-req {:SLA "Level 1"}))))
+  (let [node (-> (node-build)
+                 (node-add-requirement "SLA" "Level 1"))
+        expected-req (get-in node [:node_types :Node :requirements])]
+    (is (= expected-req {:SLA "Level 1"}))))
 
 (deftest test-adding-one-capability
-  (let [node (-> (node/build)
-                 (node/add-capability "hdd" "500gb"))
-        added-cap (->> (:node_types node)
-                       (:Node)
-                       (:capabilities))]
-    (is (= added-cap {:hdd "500gb"}))))
+  (let [node (-> (node-build)
+                 (node-add-capability "hdd" "500gb"))
+        expected-cap (get-in node [:node_types :Node :capabilities])]
+    (is (= expected-cap {:hdd "500gb"}))))
 
 (deftest test-add-one-interface
-  (let [node (-> (node/build)
-                 (node/add-interface "static" "true" ))
-        added-inf (->> (:node_types node)
-                       (:Node)
-                       (:interfaces))]
-    (is (= added-inf {:static "true"}))))
+  (let [node (-> (node-build)
+                 (node-add-interface "static" "true" ))
+        expected-inf (get-in node [:node_types :Node :interfaces])]
+    (is (= expected-inf {:static "true"}))))
 
 (deftest test-add-one-artifact
-  (let [node (-> (node/build)
-                 (node/add-artifact "purpose" "test"))
-        added-art (->> (:node_types node)
-                       (:Node)
-                       (:artifacts))]
-    (is (= added-art {:purpose "test"}))))
+  (let [node (-> (node-build)
+                 (node-add-artifact "purpose" "test"))
+        expected-art (get-in node [:node_types :Node :artifacts])]
+    (is (= expected-art {:purpose "test"}))))
 
 (deftest test-add-two-things
-  (let [node (-> (node/build)
-                 (node/add-capability "size" "medium")
-                 (node/add-interface "network" "large"))]
+  (let [node (-> (node-build)
+                 (node-add-capability "size" "medium")
+                 (node-add-interface "network" "large"))]
     (is (= node {:node_types {:Node {:type "tosca.nodes.Compute", :properties {}, :attributes {}, :requirements {}, :capabilities {:size "medium"}, :interfaces {:network "large"}, :artifacts {}, :metadatas {}}}}))))
 
 (deftest test-add-metadata
-  (let [node (-> (node/build)
-                 (node/add-metadata "name" "r2EW9012"))
-        added-data (->> (:node_types node)
-                        (:ServerNode)
-                        (:metadatas))]
+  (let [node (-> (node-build)
+                 (node-add-metadata "name" "r2EW9012"))
+        added-data (get-in node [:node_types :ServerNode :metadata])]
     (is (= node {:node_types {:Node {:type "tosca.nodes.Compute", :properties {}, :attributes {}, :requirements {}, :capabilities {}, :interfaces {}, :artifacts {}, :metadatas {:name "r2EW9012"}}}}))))
 
 (deftest test-adding-nil-value
-  (let [node (node/build)
-        empty-node (node/add-property node "something" nil)]
+  (let [node (node-build)
+        empty-node (node-add-property node "something" nil)]
     (is (= node empty-node))))
      
-(deftest test-different-node
-  (let [node (node/build "tosca.nodes.S3Storage")]
+(deftest test-different-node-type
+  (let [node (node-build "tosca.nodes.S3Storage")]
     (is (= node {:node_types {:Node {:type "tosca.nodes.S3Storage", :properties {}, :attributes {}, :requirements {}, :capabilities {}, :interfaces {}, :artifacts {}, :metadatas {}}}}))))
